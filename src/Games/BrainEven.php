b9baefd39f78d80631bdea76dfbc3077a728f6ff
BuyanauskasAA <Buyanauskas@gmail.com>
2024-02-06 15:58:09 +0600

    refactor: adjust BrainEven.php

--- src/Games/BrainEven.php
+++ src/Games/BrainEven.php
@@ -9,10 +9,12 @@ function playBrainEven(): void
     $rules = 'Answer "yes" if the number is even, otherwise answer "no".';
     $makeQuestion = function (): array {
         $num = rand(1, 100);
+
+        $question = $num;
         $answer = $num % 2 === 0 ? 'yes' : 'no';
 
         return [
-            'question' => $num,
+            'question' => $question,
             'answer' => $answer
         ];
     };
