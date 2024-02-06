003c9e199fc9b8e3785bfd4391813685c624cfda
BuyanauskasAA <Buyanauskas@gmail.com>
2024-02-06 17:02:00 +0600

    refactor: adjust BrainGcd.php

--- src/Games/BrainGcd.php
+++ src/Games/BrainGcd.php
@@ -11,7 +11,7 @@ function playBrainGcd(): void
     $makeQuestion = function (): array {
         $num1 = rand(1, 100);
         $num2 = rand(1, 100);
-        
+
         $question = "{$num1} {$num2}";
         $answer = (string) getGcd($num1, $num2);
 
